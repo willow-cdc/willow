c2b62192deb2938f7ce95785523f9e1eef160791
AlessioNovi <alessionovi@yahoo.com>
2024-03-14 13:17:59 +0100

    feat: Created initial routes for handling source connector behaviour
    route /verify for testing db connection and return an object container schemas/tables/columns info
    route /source  which handles connector setup to kafka connect
    
    Also created a general error handler in the index.ts file and a general http error class. Some refactoring and polishing still to be done.
    
    Arising issues: when initianing a connector, if the db has an existing replication slot name, it will create the connector istance and reply with a 200 code but fails gracefully inside kafka connect.
    
    Need to reach a consensus on how to define a topic prefix as well.

--- backend/src/routes/sourceRoutes.ts
+++ backend/src/routes/sourceRoutes.ts
@@ -1,64 +1,47 @@
 // routes for managing/checking/setting up source database connections
-// import pg from 'pg';
 import express from 'express';
+import { SourceRequestBody } from './types';
+import { HttpError, extractDbInfo, setupConnectorPayload } from '../utils/utils';
+import { Client } from 'pg';
+import axios from 'axios';
+
 const router = express.Router();
 
-// route for checking that the database is accessible
-
-// REQUEST
-// METHOD: POST
-// BODY: client sends source info as JSON with the following:
-// connection name
-// server IP address
-// PORT number
-// Database user
-// Database password
-
-// RESPONSE
-// if incorrect credentials or just can't work for some reason, return 401
-// if correct, return 200 with JSON of all the tables, columns, schema back to user
-// Do NOT save the info yet in the backend (on some db). We just want to ping the remote DB to ensure the connection works.
-
-router.post('/source', async (req, res) => {
-  const source = req.body;
-
-  // needs to look like this (this should be the structure we get back from the client):
-  // const client = new Client({
-  //   user: 'username',
-  //   password: 'password',
-  //   host: 'host',
-  //   port: 'port_number',
-  //   database: 'database_name',
-  // });
-
-  // verify the connection with the database
+router.post('/verify', async (req, res, next) => {
+  const source = req.body as SourceRequestBody;
 
   try {
-    // connect to the database
-    // retrieve tables, columns, schema
-    // respond with 200 and send the data back
-  } catch {
-    res.status(401);
-  }
+    const client = new Client({
+      host: source.host,
+      port: Number(source.port),
+      database: source.dbName,
+      user: source.user,
+      password: source.password,
+    });
 
-  //respond appropriately
+    // await testSourceConnection(client);
+    const data = await extractDbInfo(client, source.dbName);
+    await client.end();
+    res.json({ data });
+  } catch (error) {
+    if (error instanceof Error) {
+      const err = new HttpError(400, `Verification failed with error: ${error.message}`);
+      next(err);
+    }
+  }
 });
 
-// route for setting up database connection
+router.post('/connect', async (req, res) => {
+  const source = req.body as SourceRequestBody;
 
-// REQUEST
-// METHOD: POST
-// BODY: client sends info on what tables and other settings for source connector
-// name of connection
-// server IP address
-// PORT
-// Database user
-// Database password
-// name of tables of interest
-// which columns for each table (?)
-
-// RESPONSE
-// if incorrect, return 400. Could not set up the source DB connector.
-// if correct (was able to set up the source DB connector), create the kafka connect connection. return 200 and give some positive message affirming so.
+  try {
+    const kafkaConnectPayload = setupConnectorPayload(source);
+    const { data } = await axios.post('http://connect:8083/connectors/', kafkaConnectPayload);
+    console.log(data);
+    res.json({ data });
+  } catch {
+    res.status(401).end(); //needs refactor
+  }
+});
 
 export default router;
---
+++ backend/src/utils/utils.ts
@@ -0,0 +1,73 @@
+import { Client } from 'pg';
+import { SourceRequestBody } from '../routes/types';
+
+// export const testSourceConnection = async (client: Client) => {
+//   await client.connect();
+//   await client.end();
+//   console.log('Connection attempt succesful');
+// };
+
+export const extractDbInfo = async (client: Client, dbName: string) => {
+  await client.connect();
+
+  const schemaTextQuery =
+    "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT LIKE 'pg%' AND schema_name NOT LIKE 'information_schema' AND catalog_name LIKE $1";
+  const schemaQueryValue = [dbName];
+  const schemaQueryResult = await client.query(schemaTextQuery, schemaQueryValue);
+
+  const resultArr = schemaQueryResult.rows;
+
+  for (let i = 0; i < resultArr.length; i += 1) {
+    const currentSchema = resultArr[i];
+
+    const tableTextQuery =
+      "SELECT table_name FROM information_schema.tables WHERE table_schema=$1 AND table_type='BASE TABLE'";
+    const tableQueryValue = [currentSchema.schema_name];
+    const tableQueryResult = await client.query(tableTextQuery, tableQueryValue);
+    currentSchema.tables = tableQueryResult.rows;
+
+    for (let j = 0; j < currentSchema.tables.length; j += 1) {
+      const currentTable = currentSchema.tables[j];
+
+      const columnTextQuery =
+        'select column_name from information_schema.columns where table_name = $1 and table_schema = $2';
+      const columnQueryValue = [currentTable.table_name, currentSchema.schema_name];
+      const columnQueryResult = await client.query(columnTextQuery, columnQueryValue);
+      const columns = columnQueryResult.rows.map((columObj) => columObj.column_name);
+      currentTable.columns = columns;
+    }
+  }
+
+  return resultArr;
+};
+
+export const setupConnectorPayload = (source: SourceRequestBody) => {
+  const connectorObj = {
+    name: source.connectionName,
+    config: {
+      'plugin.name': 'pgoutput',
+      'connector.class': 'io.debezium.connector.postgresql.PostgresConnector',
+      'tasks.max': '1',
+      'database.hostname': source.host,
+      'database.port': source.port,
+      'database.user': source.user,
+      'database.password': source.password,
+      'database.dbname': source.dbName,
+      'topic.prefix': 'dbserver1', //must agree on how to define this for each connector!!!!
+    },
+  };
+
+  if (source.tables) {
+    connectorObj.config['table.include.list'] = source.tables.join(',');
+  }
+
+  return connectorObj;
+};
+
+export class HttpError extends Error {
+  public status: number;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
