dd60401281e572984e5a8d05e4bd35a79e7c915a
AlessioNovi <alessionovi@yahoo.com>
2024-03-17 13:42:45 +0100

    feat(ui): Initial selectDataForm data modelling from backend data to appropriate front end data format in order to work nicely with MUI switches logic.
    each table would have this kind of object on the frontend:   {
        table_name: 'test',
        schema_name: 'public',
        columns: [{column: "id", selected: true, dbzColumnValue: "public.test.id"}, {column: "data", selected: true, dbzColumnValue: "public.test.data"}],
        selected: true
      },
    
      also schema without tables will be simply ignored in the fronted.

--- frontend/src/components/SelectDataForm.tsx
+++ frontend/src/components/SelectDataForm.tsx
@@ -30,12 +30,8 @@ const mockData = {
       schema_name: "public2",
       tables: [
         {
-          table_name: "demo",
-          columns: ["id", "data"],
-        },
-        {
-          table_name: "demo2",
-          columns: ["id", "data"],
+          table_name: "demo3",
+          columns: ["id", "data", "asdasd"],
         },
       ],
     },
@@ -47,34 +43,67 @@ const mockData = {
 [
   {
     table_name: 'test',
-    schema: 'public',
-    columns: ["id", "data"],
+    schema_name: 'public',
+    columns: [{column: "id", selected: true, dbzColumnValue: "public.test.id"}, {column: "data", selected: true, dbzColumnValue: "public.test.data"}],
     selected: true
   },
-  {
-    table_name: 'test',
-    schema: 'public',
-    columns: ["id", "data"],
-    selected: true
-  },
-  {
-    table_name: 'test',
-    schema: 'public',
-    columns: ["id", "data"],
-    selected: false
-  }
 ]
 
 "table.include.list": ["public.table1", "public.table2"]
 */
 
+type ColumnObj = { column: string; selected: boolean; dbzColumnValue: string };
+
+interface SelectDataFormDataObj {
+  table_name: string;
+  schema_name: string;
+  columns: ColumnObj[];
+  selected: boolean;
+}
+
+type SelectDataFormData = SelectDataFormDataObj[];
+
 const SelectDataForm = () => {
-  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState<SelectDataFormData>([]);
+
+  const handleTableSwitchChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData((old) =>
+      old.map((obj) => {
+        if (`${obj.schema_name}.${obj.table_name}` === event.target.value) {
+          return { ...obj, selected: event.target.checked };
+        } else {
+          return obj;
+        }
+      })
+    );
+  };
 
   useEffect(() => {
     const result = [];
-  });
 
+    for (let i = 0; i < mockData.data.length; i++) {
+      const currentSchemaTables = mockData.data[i].tables;
+      for (let j = 0; j < currentSchemaTables.length; j++) {
+        const currentTable = currentSchemaTables[j];
+        result.push({
+          table_name: currentTable.table_name,
+          schema_name: mockData.data[i].schema_name,
+          columns: currentTable.columns.map((column) => {
+            return {
+              column,
+              selected: true,
+              dbzColumnValue: `${mockData.data[i].schema_name}.${currentTable.table_name}.${column}`,
+            };
+          }),
+          selected: true,
+        });
+      }
+    }
+    setFormData(result);
+  }, []);
+  console.log(formData);
   return (
     <>
       <Container maxWidth="md">
@@ -95,19 +124,26 @@ const SelectDataForm = () => {
         </Typography>
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Box height={300} sx={{ background: "#D9D9D9", borderRadius: 2 }}>
+            <Box
+              height={300}
+              sx={{ background: "#D9D9D9", borderRadius: 2, padding: 2 }}
+            >
               {/* list of tables here */}
               <FormGroup>
-                {mockData["data"].map((data) => {
-                  return data["tables"].map((table) => {
-                    return (
-                      <FormControlLabel
-                        control={<Switch />}
-                        label={table.table_name}
-                        name={`${data.schema_name} ${table.table_name}`}
-                      />
-                    );
-                  });
+                {formData.map((data) => {
+                  return (
+                    <FormControlLabel
+                      key={`${data.schema_name}.${data.table_name}`}
+                      control={
+                        <Switch
+                          checked={data.selected}
+                          onChange={handleTableSwitchChange}
+                          value={`${data.schema_name}.${data.table_name}`}
+                        />
+                      }
+                      label={data.table_name}
+                    />
+                  );
                 })}
               </FormGroup>
             </Box>
@@ -115,7 +151,7 @@ const SelectDataForm = () => {
           <Grid item xs={6}>
             <Box
               height={300}
-              sx={{ background: "#D9D9D9", borderRadius: 2 }}
+              sx={{ background: "#D9D9D9", borderRadius: 2, padding: 2 }}
             ></Box>
           </Grid>
         </Grid>
