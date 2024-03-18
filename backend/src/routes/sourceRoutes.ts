c9c99b3662922f91eb524c3c5435327f28e14cfa
Arno Cai <acai367@gmail.com>
2024-03-18 18:59:42 -0400

    feat(routes): :sparkles: Added SQL code to `/source/connect` to persist the source database information (which we stream events from) in the containerized PostgreSQL database.
    Co-authored-by: Alex Bair <alexbair143@gmail.com>

--- backend/src/routes/sourceRoutes.ts
+++ backend/src/routes/sourceRoutes.ts
@@ -33,11 +33,22 @@ router.post('/verify', async (req: TypedRequest<SourceRequestBody>, res, next) =
 router.post('/connect', async (req: TypedRequest<SourceRequestBody>, res, next) => {
   const source = req.body;
   const kafkaConnectPayload = setupConnectorPayload(source);
+  const database = new Client({
+    connectionString: 'postgres://postgres:postgres@db:5432',
+  });
 
   try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { data } = await axios.post('http://connect:8083/connectors/', kafkaConnectPayload);
     console.log(data);
+    await database.connect();
+
+    const query = 'INSERT INTO sources (name, db, tables, host, port, dbUser) VALUES ($1, $2, $3, $4, $5, $6)';
+    const values = [source.connectionName, source.dbName, source.tables, source.host, source.port, source.user];
+
+    const result = await database.query(query, values);
+    console.log(result.rows);
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     res.json({ data });
   } catch (error) {
