504e4c29420dde352575d28fb43bb57e996e68f8
Arno Cai <acai367@gmail.com>
2024-03-18 18:58:15 -0400

    feat(routes): :sparkles: Added SQL code to `/consumer/create` to persist the Redis sink information in the containerized PostgreSQL database.
    Co-authored-by: Alex Bair <alexbair143@gmail.com>

--- backend/src/routes/sinkRoutes.ts
+++ backend/src/routes/sinkRoutes.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import Redis from '../lib/redis';
 import KafkaConsumer from '../lib/consumer';
 import { TypedRequest, SinkRequestBody } from './types';
+import { Client } from 'pg';
 const router = express.Router();
 
 // check sink cache is accessible
@@ -22,9 +23,24 @@ router.post('/create', async (req: TypedRequest<SinkRequestBody>, res, next)=> {
   const {url, username, password, topics, connectionName } = req.body;
   const redis = new Redis(url, password, username);
   const consumer = new KafkaConsumer(redis, connectionName, ['kafka:9092'], connectionName);
+
+  const database = new Client({
+    connectionString: 'postgres://postgres:postgres@db:5432',
+  });
+  const query = 'INSERT INTO sinks (name, url, username, topics) VALUES ($1, $2, $3, $4)';
+  const values = [connectionName, url, username, JSON.stringify(topics)];
+
+  // const result = await database.query(query, values);
+  // console.log(result.rows);
   try {
     await redis.connect();
     await consumer.startConsumer(topics);
+
+    await database.connect();
+    const result = await database.query(query, values);
+    console.log('sink result', result);
+    console.log('sink result.row', result.rows);
+    
     res.json({ message: 'Consumer created!' });
   } catch (err) {
     next(err);
