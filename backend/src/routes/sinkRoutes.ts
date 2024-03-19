577fd4fd2cae02437c841e78fd4edf51f14ba38e
Arno Cai <acai367@gmail.com>
2024-03-18 21:57:57 -0400

    feat(routes): :sparkles: Use the DatabasePersistence class and query into it within `sinkRoutes.ts`.
    Previously, we directly queried the postgres database. We now have a class that directly handles interactions with the postgres database, providing a layer of misdirection/abstraction.
    
    Co-authored-by: Alex Bair <alexbair143@gmail.com>

--- backend/src/routes/sinkRoutes.ts
+++ backend/src/routes/sinkRoutes.ts
@@ -4,43 +4,36 @@ import express from 'express';
 import Redis from '../lib/redis';
 import KafkaConsumer from '../lib/consumer';
 import { TypedRequest, SinkRequestBody } from './types';
-import { Client } from 'pg';
+import Database from '../lib/dataPersistence';
 const router = express.Router();
 
 // check sink cache is accessible
 router.post('/check', async (req: TypedRequest<SinkRequestBody>, res, next) => {
-  const {url, username, password } = req.body;
+  const { url, username, password } = req.body;
   try {
     await Redis.checkConnection(url, password, username);
-    res.status(200).send({message: 'Connection successful.'});
+    res.status(200).send({ message: 'Connection successful.' });
   } catch (err) {
     next(err);
   }
 });
 
 // create sink cache connection
-router.post('/create', async (req: TypedRequest<SinkRequestBody>, res, next)=> {
-  const {url, username, password, topics, connectionName } = req.body;
+router.post('/create', async (req: TypedRequest<SinkRequestBody>, res, next) => {
+  const { url, username, password, topics, connectionName } = req.body;
   const redis = new Redis(url, password, username);
   const consumer = new KafkaConsumer(redis, connectionName, ['kafka:9092'], connectionName);
 
-  const database = new Client({
-    connectionString: 'postgres://postgres:postgres@db:5432',
-  });
-  const query = 'INSERT INTO sinks (name, url, username, topics) VALUES ($1, $2, $3, $4)';
-  const values = [connectionName, url, username, JSON.stringify(topics)];
-
-  // const result = await database.query(query, values);
-  // console.log(result.rows);
+  const database = new Database('postgres://postgres:postgres@db:5432');
   try {
     await redis.connect();
     await consumer.startConsumer(topics);
 
     await database.connect();
-    const result = await database.query(query, values);
+
+    const result = await database.insertSink(connectionName, url, username, JSON.stringify(topics));
     console.log('sink result', result);
-    console.log('sink result.row', result.rows);
-    
+
     res.json({ message: 'Consumer created!' });
   } catch (err) {
     next(err);
