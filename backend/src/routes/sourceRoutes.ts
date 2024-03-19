0761b1cc6dfba38b26f39b53ead947be9eff8aac
Arno Cai <acai367@gmail.com>
2024-03-18 21:58:26 -0400

    feat(routes): :sparkles: Use the DatabasePersistence class and query into it within `sourceRoutes.ts`.
    Previously, we directly queried the postgres database. We now have a class that directly handles interactions with the postgres database, providing a layer of misdirection/abstraction.
    
    Co-authored-by: Alex Bair <alexbair143@gmail.com>

--- backend/src/routes/sourceRoutes.ts
+++ backend/src/routes/sourceRoutes.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { TypedRequest, SourceRequestBody } from './types';
 import { HttpError, extractDbInfo, setupConnectorPayload } from '../utils/utils';
 import { Client } from 'pg';
+import Database from '../lib/dataPersistence';
 import axios from 'axios';
 
 const router = express.Router();
@@ -33,9 +34,9 @@ router.post('/verify', async (req: TypedRequest<SourceRequestBody>, res, next) =
 router.post('/connect', async (req: TypedRequest<SourceRequestBody>, res, next) => {
   const source = req.body;
   const kafkaConnectPayload = setupConnectorPayload(source);
-  const database = new Client({
-    connectionString: 'postgres://postgres:postgres@db:5432',
-  });
+  const database = new Database('postgres://postgres:postgres@db:5432');
+
+  const tables = source.tables ? source.tables.join(',') : undefined;
 
   try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -43,11 +44,16 @@ router.post('/connect', async (req: TypedRequest<SourceRequestBody>, res, next) 
     console.log(data);
     await database.connect();
 
-    const query = 'INSERT INTO sources (name, db, tables, host, port, dbUser) VALUES ($1, $2, $3, $4, $5, $6)';
-    const values = [source.connectionName, source.dbName, source.tables, source.host, source.port, source.user];
+    const result = await database.insertSource(
+      source.connectionName,
+      source.dbName,
+      tables,
+      source.host,
+      Number(source.port),
+      source.user
+    );
 
-    const result = await database.query(query, values);
-    console.log(result.rows);
+    console.log('The source insert result is', result);
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     res.json({ data });
