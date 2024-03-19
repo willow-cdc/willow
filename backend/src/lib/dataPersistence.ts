9117a65bb3ba502051d3211fcad7ac05950d7029
Arno Cai <acai367@gmail.com>
2024-03-18 20:22:38 -0400

    feat(dataPersistence): :sparkles: Added the `sourceExists` and `sinkExists` methods to check if a source and sink connector already exist (respectively).

--- backend/src/lib/dataPersistence.ts
+++ backend/src/lib/dataPersistence.ts
@@ -19,4 +19,16 @@ export default class Database {
   public async end() {
     await this.client.end();
   }
+
+  public async sourceExists(connectionName: string): Promise<boolean> {
+    const CHECK_CONNECTION_NAME = 'SELECT NULL FROM sources WHERE name = $1';
+    let result = await this.client.query(CHECK_CONNECTION_NAME, [connectionName]);
+    return (result.rowCount as number) > 0;
+  }
+
+  public async sinkExists(connectionName: string): Promise<boolean> {
+    const CHECK_CONNECTION_NAME = 'SELECT NULL FROM sinks WHERE name = $1';
+    let result = await this.client.query(CHECK_CONNECTION_NAME, [connectionName]);
+    return (result.rowCount as number) > 0;
+  }
 }
