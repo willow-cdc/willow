07472a4cebb431c4d3211573448031e893b143f6
Alex Bair <alexbair143@gmail.com>
2024-03-19 21:31:13 -0400

    feat(error_handling): :goal_net: Add `ValidationError` to error handling middleware.

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,11 +1,8 @@
 import {Request, Response, NextFunction} from 'express';
-import { HttpError, RedisError } from '../utils/utils';
+import { HttpError, RedisError, ValidationError } from '../utils/utils';
 
 export function errorHandler(err: unknown, _request: Request, response: Response, _next: NextFunction) {
-  if (err instanceof HttpError) {
-    const { status, message } = err;
-    response.status(status).json({ status, message });
-  } else if (err instanceof RedisError) {
+  if (err instanceof HttpError || err instanceof RedisError || err instanceof ValidationError) {
     const { status, message } = err;
     response.status(status).json({ status, message });
   } else {
