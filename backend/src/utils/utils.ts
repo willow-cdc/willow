a3919702b9cd88bee8218defe0a42f475661a849
Alex Bair <alexbair143@gmail.com>
2024-03-19 20:54:00 -0400

    feat(validation): :safety_vest: Add validation helper functions and create`ValidationError`.

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -137,3 +137,11 @@ export class NoPrimaryKeyError extends Error {
     this.status = status;
   }
 }
+
+export class ValidationError extends Error {
+  public status: number;
+  constructor(message: string) {
+    super(message);
+    this.status = 400;
+  }
+}
