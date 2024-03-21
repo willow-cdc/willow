49fc0a1031c3a30eb84b948fbcdd37feb2b25532
Alex Bair <alexbair143@gmail.com>
2024-03-21 17:25:23 -0400

    feat(ui): :sparkles: Create helper function to get a single pipeline's details from the backend.
    Co-authored-by: Alessio Novi <alessionovi@yahoo.com>
    Co-authored-by: Arno Cai <acai367@gmail.com>
    Co-authored-by: Cameron Bibb <cameron.bibb@icloud.com>

--- frontend/src/services/pipelines.tsx
+++ frontend/src/services/pipelines.tsx
@@ -1,9 +1,16 @@
 import axios from "axios";
-import { PipeLineArr } from "../types/types";
+import { PipeLineArr, PipeLineObj } from "../types/types";
 
 export const getAllPipelines = async () => {
   const { data } = await axios.get(
     "http://localhost:3000/pipelines" // this would need to be 'backend' instead of localhost in the future
   );
   return data.data as PipeLineArr;
+};
+
+export const getPipeLineById = async (id: number) => {
+  const { data } = await axios.get(
+    `http://localhost:3000/pipelines/${id}` // this would need to be 'backend' instead of localhost in the future
+  );
+  return data.data as PipeLineObj;
 };
