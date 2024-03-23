0920978637ca7cbf42653f8748ba46249e1dde45
Arno Cai <acai367@gmail.com>
2024-03-23 01:27:16 -0400

    feat(error_handling): :goal_net: Added to our custom error handler to explicitly handle axios error types.

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,21 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
-import { RedisError, ValidationError } from '../utils/utils';
+import { RedisError, ValidationError, DatabaseError } from '../utils/utils';
+import axios from 'axios';
+
+interface AxiosResponseData {
+  error_code: number;
+  message: string;
+}
 
 export function errorHandler(err: unknown, _request: Request, response: Response, _next: NextFunction) {
   if (err instanceof Error) {
     console.error(err.message);
 
-    if (err instanceof RedisError || err instanceof ValidationError) {
+    if (err instanceof RedisError || err instanceof ValidationError || err instanceof DatabaseError) {
       const { status, message } = err;
       response.status(status).json({ status, message });
-    } else if (err.message.includes("does not exist")) {
+    } else if (err.message.includes('does not exist')) {
       const status = 400;
-      response.status(status).json({ status, message: `Connecting to Postgres Error: ${err.message}`});
-    }
-    else {
+      response.status(status).json({ status, message: `Connecting to Source Postgres Error: ${err.message}` });
+    } else if (axios.isAxiosError(err)) {
+      if (err.response) {
+        const data = err.response.data as AxiosResponseData;
+        const { error_code: status, message } = data;
+        response.status(status).json({ status, message: `Axios Error: ${message}` });
+      }
+    } else {
       const status = 500;
       response.status(status).json({ status, message: 'Unknown error occurred.' });
     }
   }
-
 }
