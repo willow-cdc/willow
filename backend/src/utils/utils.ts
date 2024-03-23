7e08e594bb1416aca07ba1ed7ec39355130b5677
Arno Cai <acai367@gmail.com>
2024-03-23 01:37:24 -0400

    revert(error_handling): :fire: Removed the `HttpError` class from utils.ts

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -209,14 +209,6 @@ export const formatPipelineRows = (result: PipelineResult[]) => {
   });
 };
 
-export class HttpError extends Error {
-  public status: number;
-  constructor(status: number, message: string) {
-    super(message);
-    this.status = status;
-  }
-}
-
 export class RedisError extends Error {
   public status: number;
   constructor(status: number, message: string) {
