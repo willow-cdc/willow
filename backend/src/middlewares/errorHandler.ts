a2387debcc206f2f27b9e9afeca871673164da78
Arno Cai <acai367@gmail.com>
2024-03-23 12:52:26 -0400

    feat(error_handling): :goal_net: Added explicit logic to handle duplicate key violations within our error handler.
    Duplicate key errors would be thrown when we try to insert source or sink info into the Postgres database where the source/sink name has already been claimed.

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -19,6 +19,9 @@ export function errorHandler(err: unknown, _request: Request, response: Response
         const { error_code: status, message } = data;
         response.status(status).json({ status, message: `Axios Error: ${message}` });
       }
+    } else if (/duplicate key value violates unique constraint/.test(String(err))) {
+      const status = 400;
+      response.status(status).json({ status, message: `Postgres Query Error: ${err.message}` });
     } else {
       const status = 500;
       response.status(status).json({ status, message: 'Unknown error occurred.' });
