3ede88e6feb2269e562ee9499cda6bb5167d3fc7
Arno Cai <acai367@gmail.com>
2024-03-23 00:06:12 -0400

    feat(error_handling): :goal_net: Catch errors connecting to the database via pg.
    More specifically catch nonexistent user and database errors; errors returned via `Client.prototype.connect` will provide a error instance with a message of the form: `[database|role] [databaseName|roleName] does not exist` where `databaseName` and `roleName` are placeholders for the database and role

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -2,13 +2,20 @@ import { Request, Response, NextFunction } from 'express';
 import { RedisError, ValidationError } from '../utils/utils';
 
 export function errorHandler(err: unknown, _request: Request, response: Response, _next: NextFunction) {
-  console.error((err as Error).message);
+  if (err instanceof Error) {
+    console.error(err.message);
 
-  if (err instanceof RedisError || err instanceof ValidationError) {
-    const { status, message } = err;
-    response.status(status).json({ status, message });
-  } else {
-    const status = 500;
-    response.status(status).json({ status, message: 'Unknown error occurred.' });
+    if (err instanceof RedisError || err instanceof ValidationError) {
+      const { status, message } = err;
+      response.status(status).json({ status, message });
+    } else if (err.message.includes("does not exist")) {
+      const status = 400;
+      response.status(status).json({ status, message: `Connecting to Postgres Error: ${err.message}`});
+    }
+    else {
+      const status = 500;
+      response.status(status).json({ status, message: 'Unknown error occurred.' });
+    }
   }
+
 }
