2f17ee52234cb69026d84025381ba2b31bac657c
Arno Cai <acai367@gmail.com>
2024-03-22 23:21:12 -0400

    refactor(error_handling): :fire: Remove the `HttpError` import and call from the `errorHandler.ts` file for now.
    We are not currently using the `HttpError` class, thus remove this for now. May reinstate later once we figure out how to best handle error types that haven't been accounted for in our routes.

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,8 +1,10 @@
-import {Request, Response, NextFunction} from 'express';
-import { HttpError, RedisError, ValidationError } from '../utils/utils';
+import { Request, Response, NextFunction } from 'express';
+import { RedisError, ValidationError } from '../utils/utils';
 
 export function errorHandler(err: unknown, _request: Request, response: Response, _next: NextFunction) {
-  if (err instanceof HttpError || err instanceof RedisError || err instanceof ValidationError) {
+  console.error((err as Error).message);
+
+  if (err instanceof RedisError || err instanceof ValidationError) {
     const { status, message } = err;
     response.status(status).json({ status, message });
   } else {
