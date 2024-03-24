670d8ac221e4ae7a09e8882ae338e23e8f63c13a
AlessioNovi <alessionovi@yahoo.com>
2024-03-24 12:54:12 +0100

    style(ui): :lipstick: Adjusted tables font size on individual pipeline source card to match the rest of the details font sizing

--- frontend/src/components/Source.tsx
+++ frontend/src/components/Source.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Typography, List, ListItem, ListItemText, Box } from "@mui/material";
 import { PipeLine } from "../types/types";
 
-const Source = ({pipeLineData}: {pipeLineData: PipeLine}) => {
+const Source = ({ pipeLineData }: { pipeLineData: PipeLine }) => {
   return (
     <Box
       height={550}
-      overflow={'auto'}
-      sx={{ background: '#D9D9D9', borderRadius: 2, paddingTop: 2 }}
+      overflow={"auto"}
+      sx={{ background: "#D9D9D9", borderRadius: 2, paddingTop: 2 }}
     >
       <Typography variant="h6" align="center">
         Source
@@ -15,44 +15,48 @@ const Source = ({pipeLineData}: {pipeLineData: PipeLine}) => {
       <List>
         <ListItem>
           <ListItemText
-            primary={'Source name:'}
+            primary={"Source name:"}
             secondary={pipeLineData.source_name}
           />
         </ListItem>
         <ListItem>
           <ListItemText
-            primary={'Database:'}
+            primary={"Database:"}
             secondary={pipeLineData.source_database}
           />
         </ListItem>
         <ListItem>
           <ListItemText
-            primary={'Host:'}
+            primary={"Host:"}
             secondary={pipeLineData.source_host}
           />
         </ListItem>
         <ListItem>
           <ListItemText
-            primary={'Port name:'}
+            primary={"Port name:"}
             secondary={pipeLineData.source_port}
           />
         </ListItem>
         <ListItem>
           <ListItemText
-            primary={'User:'}
+            primary={"User:"}
             secondary={pipeLineData.source_user}
           />
         </ListItem>
         <ListItem>
           <ListItemText
-            primary={'Tables:'}
+            primary={"Tables:"}
             secondary={
               <React.Fragment>
                 {pipeLineData.tables.map((table) => {
                   return (
                     <Typography
                       key={table}
-                      sx={{ marginLeft: 3, display: 'block' }}
+                      sx={{
+                        marginLeft: 3,
+                        display: "block",
+                        fontSize: "0.875rem",
+                      }}
                       component="span"
                     >
                       {table}
