3c1da6d0dc47024ae67754e872a7be686f8ac4df
Alex Bair <alexbair143@gmail.com>
2024-03-25 17:30:45 -0400

    refactor(error_handling): :art: Refactor error handler logic to be cleaner & standardized.
    Co-authored-by: Arno Cai <acai367@gmail.com>

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -7,24 +7,27 @@ export function errorHandler(err: unknown, _request: Request, response: Response
   if (err instanceof Error) {
     console.error(err.message);
 
+    let status: number;
+    let message: string;
+
     if (err instanceof HttpError || err instanceof RedisError || err instanceof ValidationError || err instanceof DatabaseError) {
-      const { status, message } = err;
-      response.status(status).json({ status, message });
+      status = err.status;
+      message = err.message;
     } else if (err.message.includes('does not exist') || err.message.includes('password authentication failed')) {
-      const status = 400;
-      response.status(status).json({ status, message: `Connecting to Source Postgres Error: ${err.message}` });
-    } else if (axios.isAxiosError(err)) {
-      if (err.response) {
-        const data = err.response.data as AxiosResponseData;
-        const { error_code: status, message } = data;
-        response.status(status).json({ status, message: `Axios Error: ${message}` });
-      }
+      status = 400;
+      message = `Connecting to Source Postgres Error: ${err.message}`;
+    } else if (axios.isAxiosError(err) && err.response) {
+      const data = err.response.data as AxiosResponseData;
+      status = data.error_code;
+      message = `Axios Error: ${data.message}`
     } else if (/duplicate key value violates unique constraint/.test(String(err))) {
-      const status = 400;
-      response.status(status).json({ status, message: `Postgres Query Error: ${err.message}` });
+      status = 400;
+      message = `Postgres Query Error: ${err.message}`
     } else {
-      const status = 500;
-      response.status(status).json({ status, message: 'Unknown error occurred.' });
+      status = 500;
+      message = 'Unknown error occurred.';
     }
+
+    response.status(status).json({status, message});
   }
 }
