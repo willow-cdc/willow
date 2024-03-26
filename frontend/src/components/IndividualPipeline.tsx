bb0e101d3ad53eecd4a0615c277e31bbc06ee220
AlessioNovi <alessionovi@yahoo.com>
2024-03-26 11:11:52 +0100

    refactor(ui): changed file ext of pipelines services file from tsx to ts to be aligned with other files. Fixed a type issue inside IndividualPipeline component

--- frontend/src/components/IndividualPipeline.tsx
+++ frontend/src/components/IndividualPipeline.tsx
@@ -1,45 +1,44 @@
 import { Typography, Box, Container, Grid } from "@mui/material";
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { PipeLine } from '../types/types';
-import { getPipeLineById } from '../services/pipelines';
-import Source from './Source';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { PipeLine } from "../types/types";
+import { getPipeLineById } from "../services/pipelines";
+import Source from "./Source";
 import Sink from "./Sink";
 
 const IndividualPipeline = () => {
   const { pipeline_id } = useParams();
-  const [pipeLineData, setPipeLineData] = useState<PipeLine>(null);
+  const [pipeLineData, setPipeLineData] = useState<PipeLine | null>(null);
 
   useEffect(() => {
     const fetchPipeLineData = async () => {
       try {
-        const data = await getPipeLineById(Number(pipeline_id))
-        setPipeLineData(data)
+        const data = await getPipeLineById(Number(pipeline_id));
+        setPipeLineData(data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    fetchPipeLineData()
-  }, [pipeline_id])
-
+    };
+    fetchPipeLineData();
+  }, [pipeline_id]);
 
   if (!pipeLineData) {
-    return null
+    return null;
   }
-  
+
   return (
-    <Container sx={{ mt: '5%' }}>
-      <Box sx={{ textAlign: 'center', mb: '20px' }}>
-        <Typography component={'h1'} variant={'h5'}>
+    <Container sx={{ mt: "5%" }}>
+      <Box sx={{ textAlign: "center", mb: "20px" }}>
+        <Typography component={"h1"} variant={"h5"}>
           Pipeline {pipeLineData.pipeline_id}
         </Typography>
       </Box>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Source pipeLineData={pipeLineData}/>
+          <Source pipeLineData={pipeLineData} />
         </Grid>
         <Grid item xs={6}>
-          <Sink pipeLineData={pipeLineData}/>
+          <Sink pipeLineData={pipeLineData} />
         </Grid>
       </Grid>
     </Container>
